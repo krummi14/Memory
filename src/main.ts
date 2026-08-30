2dee56bfb3dd84218608907ded6651e42e8f9860
Daniel Krummhauer <d.krummhauer@gmail.com>
2026-08-30 20:50:47 +0200

    feat: memory card flip methode

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
 import './styles/style.scss';
-
 //document.getElementById('h1_memory')!.innerText = "first thread";
+
+init();
+
+function init () {
+    const fieldRef = document.getElementById('field');
+    if (fieldRef) {
+        fieldRef.addEventListener('click', e => {
+            const card = (e.target as HTMLElement).closest('.card') as HTMLButtonElement;
+            if(card) {
+                card.classList.toggle('is_flipped');
+            }
+        })
+    }
+}
